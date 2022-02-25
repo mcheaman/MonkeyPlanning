1a703052f1ada9b29eba8bab4281e76b73d271f6
Mason Heaman <heamanm@cf164-02.cs.wwu.edu>
2022-02-24 21:25:49 -0800

    Move and worldstate basic functions

--- WorldState.java
+++ WorldState.java
@@ -6,14 +6,32 @@ public class WorldState {
     private boolean monkeyHasBananas;
 
     public static final String ROOM_A = "A";
-    
+    public static final String ROOM_B = "B";
+    public static final String ROOM_C = "C";
+    public static final String HEIGHT_LOW = "Low";
+    public static final String HEIGHT_HIGH = "High";
+
     public String getRoomMonkeyIn(){
         return this.roomMonkeyIn;
+    }
+    public String getRoomBoxIn(){
+        return this.roomBoxIn;
+    }
+    public String roomBananasIn(){
+        return this.roomBananasIn;
+    }
+
+    public boolean getMonkeyHasBananas(){
+        return this.monkeyHasBananas;
     }
 
     public boolean isMonkeyAt(String room) {
         return this.roomMonkeyIn.equalsIgnoreCase(room);
     }
+    public boolean isMonkeyHeight(String height) {
+        return this.monkeyHeight.equalsIgnoreCase(height);
+    }
+
 
 }
 
